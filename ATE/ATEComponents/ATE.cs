688bca5275def1be7ca8fc0097b7846bbaab4e06
Vlad Mozder <rekeloz@gmail.com>
2017-11-25 20:51:53 +0300

    add base ATE calss: ATE,Terminal,Port. Add some data structure class: CallData,CustomerData,Rate

---
+++ ATE/ATEComponents/ATE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATE.CustomDataTypes.Emums;
+using ATE.CustomDataTypes;
+namespace ATE.ATEComponents
+{
+    class ATE
+    {
+        private Dictionary<int, Tuple<Port, CustomerData>> customerData;
+        private List<CallData> callList = new List<CallData>();
+
+        public Terminal AddATEUser(string firstName,string lastName, RateList rate)
+        {
+            CustomerData Customer = new CustomerData(firstName, lastName, rate);
+            Port Port = new Port();
+            //Port.CallEvent += CallNumber;
+            //Port.AnswerEvent += AnswerNumber;          
+            //Port.EndCallEvent += EndCallNumber;
+            customerData.Add(Customer.Number, new Tuple<Port, CustomerData>(Port, Customer));
+            var newTerminal = new Terminal(Customer.Number, Port);
+            return newTerminal;
+        }
+               
+    }
+}
